e24992addc14985e4ab7dc240e15091bf739bbdc
Maheshsumb <maheshsumbpatil87@gmail.com>
2024-02-29 19:50:53 +0530

    Given Functionality to get started button

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const signupBtn = document.getElementById('signupBtn');
     const loginBtn = document.getElementById('loginBtn');
+    const getStartedBtn = document.getElementById('getStartedBtn');
 
     // Add event listeners to the buttons
     signupBtn.addEventListener('click', function () {
         // Redirect the user to the signup page
-        window.location.href = '/register.html'; // Replace with your signup page URL
+        window.location.href = '/register.html'; 
     });
 
     loginBtn.addEventListener('click', function () {
         // Redirect the user to the login page
-        window.location.href = '/login.html'; // Replace with your login page URL
+        window.location.href = '/login.html'; 
+    });
+    getStartedBtn.addEventListener('click', function () {
+        window.location.href ='/register.html'; 
     });
 });
